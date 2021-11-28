58abce32ca285565ba00e1ab5029053002927106
yunhes45 <yunhes45@gmail.com>
2021-11-28 20:09:34 +0900

    [#21] feat : POS 메뉴 편집(insert)

--- menuedit_logic.py
+++ menuedit_logic.py
@@ -1,2 +1,23 @@
-def menuedit():
-    print("hi")
+import pymysql
+from flask import request, redirect
+from werkzeug.utils import secure_filename
+import uuid
+
+import os
+
+db = pymysql.connect(host='localhost', user='root', password='1234', db='kiosk', charset='utf8')
+cursor = db.cursor()
+
+def menuedit(menuname, menuprice, menuimg):
+    sql = "INSERT INTO menu (menu_name, menu_price) VALUES (%s, %s);"
+    cursor.execute(sql, (menuname, menuprice))
+    db.commit()
+    
+    res = cursor.lastrowid
+    menuimg.save('static/menuimg/' + str(uuid.uuid4()) + secure_filename(menuimg.filename))
+    menuimg_files = os.listdir("static/menuimg/")
+
+    sql1 = "INSERT INTO menu_img (file_name, original_filename, file_url, userid) VALUES(%s, %s, %s, %s);"
+    cursor.execute(sql1, (str(uuid.uuid4()) + secure_filename(menuimg.filename), secure_filename(menuimg.filename), 'menuimg/'+secure_filename(menuimg.filename), res))
+    db.commit()
+
--- server.py
+++ server.py
@@ -1,16 +1,29 @@
-from flask import Flask #flask 모듈 import 
-from flask import render_template
+import pymysql
+from flask import Flask, render_template, request, redirect
+from werkzeug.utils import secure_filename
 import menuedit_logic
 
 app = Flask(__name__) # flask name 선언 
+
+db = pymysql.connect(host='localhost', user='root', password='1234', db='kiosk', charset='utf8')
+cursor = db.cursor()
 
 @app.route("/") #flask 웹 페이지 경로 
 def main(): # 경로에서 실행될 기능 선언 
     return render_template('Pos_main.html')
 
-@app.route('/menuedit')
+@app.route('/menuedit', methods=['GET', 'POST'])
 def menuedit():
-    menuedit_logic.menuedit()
+    if request.method == 'POST':
+        menu_info = request.form
+        menuname = menu_info['menu_nameTxt']
+        menuprice = menu_info['menu_priceTxt']
+
+        menu_img = request.files
+        menuimg = menu_img['menu_img']
+
+        menuedit_logic.menuedit(menuname, menuprice, menuimg)
+    
     return render_template('Pos_menuEdit.html')
     
 @app.route('/calc')
